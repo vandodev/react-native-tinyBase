49d1ebb573b629eb2fa45249c3a8857676a636ba
vandodev <vandoaparecido@hotmail.com>
2025-03-20 13:07:42 -0300

    :game_die: :rocket: :game_die: Adicionando dados com tinybase

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,14 +1,43 @@
-import { useState } from "react"
-import { View, Text, FlatList } from "react-native"
+import { useEffect, useState } from "react"
+import { View, Text, FlatList, Alert } from "react-native"
 
 import { styles } from "./styles"
 import { Item } from "@/components/Item"
 import { Input } from "@/components/Input"
 import { Button } from "@/components/Button"
 
+//Tinybase
+import { createStore } from "tinybase"
+
+
+const TABLE_NAME = "products"
+const store = createStore()
+
+
 export function Home() {
   const [description, setDescription] = useState("")
   const [products, setProducts] = useState([])
+
+  function get(){
+    const data = store.getTable(TABLE_NAME)
+    console.log(data)
+  }
+
+  function add(){
+    if(description.trim() === ""){
+      return Alert.alert("Atenção", "Informe o produto")
+    }
+    //"1234":{} formato de dados, o id fica de fora
+    const id = Math.random().toString(30).substring(2,20) //gera o id
+    // console.log(id)
+    store.setRow(TABLE_NAME, id, {description, done: false})
+    setDescription("")
+    get()
+  }
+
+  useEffect(() => {
+    get();
+  },[])
 
   return (
     <View style={styles.container}>
@@ -19,7 +48,7 @@ export function Home() {
           value={description}
         />
 
-        <Button title="Adicionar" />
+        <Button title="Adicionar" onPress={add}/>
       </View>
 
       <FlatList
